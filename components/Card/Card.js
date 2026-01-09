01b158659e80a08cae8685994c731cb279cbd7ed
Mim-Sky <horodyski.michal@gmail.com>
2026-01-09 15:16:11 +0000

    Integracja z sanity.io - pobieranie danych o książkach

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,14 +1,21 @@
-import { books } from "../../config/books";
+import { urlFor } from "../../lib/sanity";
 
-export function Card({ }) {
+export function Card({ books }) {
+  if (!books || books.length === 0) {
+    return null;
+  }
+
   return (
     <div className="">
       <h2>Books</h2>
       {books.map((book) => (
-        <div key={book.title}>
-          <h3>{book.title}</h3>
-          <p>{book.description}</p>
-					<img alt="Book title" src={book.image} />
+        <div key={book._id}>
+          <h3>{book.name}</h3>
+          {book.author && <p>{book.author}</p>}
+          {book.description && <p>{book.description}</p>}
+          {book.image && (
+            <img alt={book.name} src={urlFor(book.image).width(400).url()} />
+          )}
         </div>
       ))}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,15 @@
-import { createClient } from "next-sanity";
+import { client, booksQuery } from "../lib/sanity";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 import { Contact } from "../components/Contact/Contact";
 import { Card } from "../components/Card/Card";
-// import {books} from "../config/books";
 
 export default function IndexPage({ books }) {
   return (
     <>
       <main>
         <Header />
-        {/* {books.length > 0 && (
-          <section>
-            <h2>Books</h2>
-            <div className="books__wrapper">
-              {books.map((book) => (
-                <div className="book">
-                  <p className="book__title" key={book._id}>
-                    {book?.name}
-                  </p>
-                  <p className="book__author" key={book._id}>
-                    {book?.author}
-                  </p>
-                  <img alt="book cover" src={book.image} key={book._id} />
-                </div>
-              ))}
-            </div>
-          </section>
-        )}
-        {!books.length > 0 && <p>No books to show</p>} */}
-        <Card />
+        <Card books={books} />
         <Contact />
         <Footer />
       </main>
@@ -37,18 +17,13 @@ export default function IndexPage({ books }) {
   );
 }
 
-const client = createClient({
-  projectId: "kzn8exmp",
-  dataset: "production",
-  apiVersion: "2023-09-14",
-  useCdn: false,
-});
 export async function getStaticProps() {
-  const books = await client.fetch(`*[_type == "book"]`);
+  const books = await client.fetch(booksQuery);
 
   return {
     props: {
       books,
     },
+    revalidate: 60, // Revalidate every 60 seconds
   };
 }
