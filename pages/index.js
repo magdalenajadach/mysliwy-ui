e9399aaa84710fccc1771cacd2d303d7e7618c78
Mim-Sky <horodyski.michal@gmail.com>
2026-01-09 15:17:53 +0000

    .

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
-import { client, booksQuery } from "../lib/sanity";
+import { createClient } from "next-sanity";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 import { Contact } from "../components/Contact/Contact";
 import { Card } from "../components/Card/Card";
+// import {books} from "../config/books";
 
 export default function IndexPage({ books }) {
   return (
     <>
       <main>
         <Header />
-        <Card books={books} />
+        {/* {books.length > 0 && (
+          <section>
+            <h2>Books</h2>
+            <div className="books__wrapper">
+              {books.map((book) => (
+                <div className="book">
+                  <p className="book__title" key={book._id}>
+                    {book?.name}
+                  </p>
+                  <p className="book__author" key={book._id}>
+                    {book?.author}
+                  </p>
+                  <img alt="book cover" src={book.image} key={book._id} />
+                </div>
+              ))}
+            </div>
+          </section>
+        )}
+        {!books.length > 0 && <p>No books to show</p>} */}
+        <Card />
         <Contact />
         <Footer />
       </main>
@@ -17,13 +37,18 @@ export default function IndexPage({ books }) {
   );
 }
 
+const client = createClient({
+  projectId: "kzn8exmp",
+  dataset: "production",
+  apiVersion: "2023-09-14",
+  useCdn: false,
+});
 export async function getStaticProps() {
-  const books = await client.fetch(booksQuery);
+  const books = await client.fetch(`*[_type == "book"]`);
 
   return {
     props: {
       books,
     },
-    revalidate: 60, // Revalidate every 60 seconds
   };
 }
